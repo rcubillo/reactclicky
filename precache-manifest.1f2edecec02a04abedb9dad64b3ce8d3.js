b0bb8b6f6446622ecf44e910bf0c5ada53682cf9
Chino <ronaldcubillo92@gmail.com>
2019-06-03 11:17:46 -0500

    Updates

---
+++ precache-manifest.1f2edecec02a04abedb9dad64b3ce8d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d00bda9eb499f8e11bb78133cd66eea",
+    "url": "/reactclicky/index.html"
+  },
+  {
+    "revision": "5dac29f4668633615263",
+    "url": "/reactclicky/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactclicky/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5dac29f4668633615263",
+    "url": "/reactclicky/static/js/main.68fe3ae5.chunk.js"
+  },
+  {
+    "revision": "3bfadf5264b4a2d47930",
+    "url": "/reactclicky/static/js/runtime~main.fd0a69df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactclicky/static/media/logo.5d5d9eef.svg"
+  }
+]);
