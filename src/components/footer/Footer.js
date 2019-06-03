cbb5e1c8b7bb1f149aed152e9a60b0b20b3119d4
Chino <ronaldcubillo92@gmail.com>
2019-06-03 16:20:10 -0500

    Added a footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => <div id="mainfooter" className="footer text-center">Clicky Game by Ronald Cubillo</div>;
+
+export default Footer;
